c1b287212a389d8cb2eb274a05304fdc1fdd9620
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-01-14 17:49:19 +0800

    feat(app): added bounds check logic

--- Main.java
+++ Main.java
@@ -7,6 +7,10 @@ public class Main {
         System.out.println("~Roguelike 2~");
         System.out.println("Use WASD to move, Q to quit.");
         Integer[] userStartPosition = {0,0};
+        Integer minX = 0;
+        Integer maxX = 10;
+        Integer minY = 0;
+        Integer maxY = 10;
 
         String input = readUserInput();
 
@@ -14,9 +18,9 @@ public class Main {
 
             System.out.println(input);
             System.out.println(userStartPosition[0] + ", " + userStartPosition[1]);
-            moveUser(input, userStartPosition);
+            moveUser(input, userStartPosition, minX, maxX, minY, maxY);
             input = readUserInput();
-            drawScreen(createScreen(userStartPosition));
+            drawScreen(createScreen(userStartPosition, minX, maxX, minY, maxY));
 
         } 
 
@@ -28,19 +32,27 @@ public class Main {
         return userInput;
     }
 
-    public static Integer[] moveUser(String input, Integer[] userPosition){
+    public static Integer[] moveUser(String input, Integer[] userPosition, Integer minX, Integer maxX, Integer minY, Integer maxY){
         switch (input){
             case "w": // up
-                userPosition[1] += 1;
+                if (checkBounds(userPosition[0], userPosition[1] + 1, minX, maxX, minY, maxY)){
+                    userPosition[1] += 1;
+                }
                 break;
             case "a": // left
-                userPosition[0] -= 1;
+                if (checkBounds(userPosition[0] - 1, userPosition[1], minX, maxX, minY, maxY)){
+                    userPosition[0] -= 1;
+                }
                 break;
             case "s": // down
-                userPosition[1] -= 1;
+                if (checkBounds(userPosition[0], userPosition[1] - 1, minX, maxX, minY, maxY)){
+                    userPosition[1] -= 1;
+                }
                 break;
             case "d": // right
-                userPosition[0] += 1;
+                if (checkBounds(userPosition[0] + 1, userPosition[1], minX, maxX, minY, maxY)){
+                    userPosition[0] += 1;
+                }
                 break;
             default:
                 System.out.println("Invalid input: " + input);
@@ -49,15 +61,13 @@ public class Main {
         return userPosition;     
     }
 
-    public static String[] createScreen(Integer[] userStartPosition){
+    public static String[] createScreen(Integer[] userStartPosition, Integer minX, Integer maxX, Integer minY, Integer maxY){
         Integer userX = userStartPosition[0];
         Integer userY = userStartPosition[1];
-        Integer maxX = 10;
-        Integer maxY = 10;
         String rowBuffer = "";
         String[] screenBuffer = new String[10];
-        for (int y=0; y<maxY; y++){
-            for (int x=0; x<maxX; x++){
+        for (int y=minY; y<maxY; y++){
+            for (int x=minX; x<maxX; x++){
                 if (x == userX && y == userY){
                     rowBuffer += "P";
                 } else {
@@ -73,6 +83,10 @@ public class Main {
         for (int i=0; i<screenBuffer.length; i++){
             System.out.println(screenBuffer[i]);
         }
+    }
+
+    public static boolean checkBounds(Integer x, Integer y, Integer minX, Integer maxX, Integer minY, Integer maxY){
+        return (x >= minX && x <= maxX && y >= minY && y <= maxY);
     }
 
 }
