14bed8b26c96e7b853aff3fb4e117adb17489807
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-01-14 17:44:14 +0800

    feat(app): thenway you move

--- Main.java
+++ Main.java
@@ -5,15 +5,19 @@ public class Main {
     public static void main(String[] args){
 
         System.out.println("~Roguelike 2~");
+        System.out.println("Use WASD to move, Q to quit.");
         Integer[] userStartPosition = {0,0};
 
         String input = readUserInput();
+
         while (!input.equals('q')){
+
             System.out.println(input);
-            System.out.println(userStartPosition);
+            System.out.println(userStartPosition[0] + ", " + userStartPosition[1]);
             moveUser(input, userStartPosition);
             input = readUserInput();
-            drawScreen(userStartPosition);
+            drawScreen(createScreen(userStartPosition));
+
         } 
 
     }
@@ -45,7 +49,7 @@ public class Main {
         return userPosition;     
     }
 
-    public static String[] drawScreen(Integer[] userStartPosition){
+    public static String[] createScreen(Integer[] userStartPosition){
         Integer userX = userStartPosition[0];
         Integer userY = userStartPosition[1];
         String rowBuffer = "";
@@ -61,6 +65,12 @@ public class Main {
             screenBuffer[y] = rowBuffer;
         }
         return screenBuffer;
+    }
+
+    public static void drawScreen(String[] screenBuffer){
+        for (int i=0; i<screenBuffer.length; i++){
+            System.out.println(screenBuffer[i]);
+        }
     }
 
 }
