5aa60e92e50e59fcaa27890e4e17fe8d3ffbbd69
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-01-14 17:26:42 +0800

    feat(app): basic game loop added

--- Main.java
+++ Main.java
@@ -3,9 +3,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args){
+
         System.out.println("~Roguelike 2~");
+        Integer[] userStartPosition = {0,0};
+
         String input = readUserInput();
-        System.out.println(input);
+        while (input != "q"){
+            System.out.println(input);
+            System.out.println(userStartPosition);
+            moveUser(input, userStartPosition);
+            input = readUserInput();
+        } 
+
     }
 
     public static String readUserInput(){
@@ -13,6 +22,27 @@ public class Main {
         String userInput = sc.next();
         sc.close();
         return userInput;
+    }
+
+    public static Integer[] moveUser(String input, Integer[] userPosition){
+        switch (input){
+            case "w": // up
+                userPosition[1] += 1;
+                break;
+            case "a": // left
+                userPosition[0] -= 1;
+                break;
+            case "s": // down
+                userPosition[1] -= 1;
+                break;
+            case "d": // right
+                userPosition[0] += 1;
+                break;
+            default:
+                System.out.println("Invalid input: " + input);
+                break;
+        }
+        return userPosition;     
     }
 
 }
