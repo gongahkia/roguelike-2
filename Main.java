99cd020784d77823ae825fb9c212b5b959bdf3b2
Gabriel Ong Zhe Mian <angryapplegravy@gmail.com>
2026-01-14 17:20:28 +0800

    feat(app): intiialise scrit

--- Main.java
+++ Main.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
-    
+
+    public static void main(String[] args){
+        System.out.println("~Roguelike 2~");
+        String input = readUserInput();
+        System.out.println(input);
+    }
+
+    public static String readUserInput(){
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.next();
+        sc.close();
+        return userInput;
+    }
+
 }
